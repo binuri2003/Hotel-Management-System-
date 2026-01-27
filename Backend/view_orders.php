35ebc7f024edf1fdf4c256d7d5d6e248d5a3dd4e
rashmi-pw <rashiwijesinghe04@gmail.com>
2026-01-27 15:30:20 +0530

    Add food reservation backend

---
+++ Backend/view_orders.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include 'config.php';
+
+if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'admin') {
+    die("Access denied. Admins only.");
+}
+
+$sql = "SELECT * FROM `order`";
+$result = mysqli_query($conn, $sql);
+?>
+
+<h2>Food Orders</h2>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>Room ID</th>
+        <th>Order Date & Time</th>
+        <th>Order Total</th>
+    </tr>
+
+    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td><?php echo $row['RoomID']; ?></td>
+            <td><?php echo $row['OrderDateTime']; ?></td>
+            <td><?php echo $row['OrderTotal']; ?></td>
+        </tr>
+    <?php } ?>
+</table>
+
